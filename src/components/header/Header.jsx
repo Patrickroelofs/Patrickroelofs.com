fdb8278c4a2300a9fa37768b623439b023d6ff16
Patrick Roelofs <contact@patrickroelofs.com>
2021-01-13 23:44:11 +0100

    fix skew being too much

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,10 @@ function Header() {
   return (
     <header className="relative w-screen h-screen">
       <div className="absolute top-1/2 w-screen" style={{ transform: 'translateY(-50%)' }}>
-        <span style={{ transform: 'skew(15deg, 0) rotate(-8deg)' }} className="inline-block">
+        <span style={{ transform: 'rotate(-8deg)' }} className="inline-block">
           <h1 style={{ fontSize: '23vw', transform: 'skew(25deg, 0)' }} className="font-headers font-black uppercase leading-none">Patrick</h1>
         </span>
-        <span style={{ transform: 'skew(15deg, 0) rotate(-8deg)' }} className="inline-block">
+        <span style={{ transform: 'rotate(-8deg)' }} className="inline-block">
           <h1 style={{ fontSize: '22vw', transform: 'skew(25deg, 0)' }} className="font-headers font-black uppercase -top-8 relative leading-none">Roelofs</h1>
         </span>
       </div>
