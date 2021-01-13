a6eb5cb60c5a14808ad9c8af1d5ede986ae89480
Patrick Roelofs <contact@patrickroelofs.com>
2021-01-13 22:26:38 +0100

    add header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <main className="bg-theme 100vw min-h-screen">
+      <header className="relative overflow-hidden w-screen h-screen">
+        <div className="absolute top-1/2 w-screen" style={{ transform: 'translateY(-50%)' }}>
+          <h1 style={{ fontSize: '23vw', transform: 'skew(25deg, 0)' }} className="font-headers font-black uppercase leading-none">Patrick</h1>
+          <h1 style={{ fontSize: '22vw', transform: 'skew(25deg, 0)' }} className="font-headers font-black uppercase -top-8 relative leading-none">Roelofs</h1>
+        </div>
+      </header>
+    </main>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      theme: '#fefdf8',
+    },
+    fontFamily: {
+      headers: ['Work Sans', 'sans-serif'],
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
