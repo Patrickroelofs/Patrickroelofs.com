fc26d0d207ef60b7c12f8842d3a0ddf558de4537
Patrick Roelofs <contact@patrickroelofs.com>
2021-01-14 22:42:07 +0100

    add .env check on server

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'))
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'))
+  });
+}
 
 app.listen(5000, () => {
   console.log('App listening on port 5000!')
