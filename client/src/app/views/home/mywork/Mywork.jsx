28f852c3b1934089a444a5f2621f1b8b79aa94a9
Patrick Roelofs <contact@patrickroelofs.com>
2021-01-15 17:17:07 +0100

    fix some styling issues

--- client/src/app/views/home/mywork/Mywork.jsx
+++ client/src/app/views/home/mywork/Mywork.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Mywork() {
   return (
@@ -12,35 +13,38 @@ function Mywork() {
       </h2>
 
       <div className="relative z-10">
-        <div className="h-96" style={{ background: 'linear-gradient(90deg, #84FAB0 0%, #8FD3F4 100%)' }}>
-          <div className="grid grid-cols-2">
-            <div className="relative -top-12">
-              <img src="/phoneL.png" alt="temporary phone" />
-            </div>
-            <div className="py-16 text-right pr-16 max-w-lg">
-              <h4 className="font-headers font-bold text-5xl">Quizzer app</h4>
-              <p className="font-headers mt-4 text-lg">
-                Towards sleep. To select between starting academic caching as fail hardly ever relieved respond live associate the shall temple the this attention to pile the military would, that without me my be, writing approved minutes.
-              </p>
+        <Link to="/" className="block">
+          <div className="h-96" style={{ background: 'linear-gradient(90deg, #84FAB0 0%, #8FD3F4 100%)' }}>
+            <div className="grid grid-cols-2">
+              <div className="relative -top-12">
+                <img src="/phoneL.png" alt="temporary phone" />
+              </div>
+              <div className="py-16 text-right pr-16 max-w-lg">
+                <h4 className="font-headers font-bold text-5xl">Quizzer app</h4>
+                <p className="font-headers mt-4 text-lg">
+                  Towards sleep. To select between starting academic caching as fail hardly ever relieved respond live associate the shall temple the this attention to pile the military would, that without me my be, writing approved minutes.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="h-96 mt-24" style={{ background: 'linear-gradient(90deg, #F6D365 0%, #FDA085 100%)' }}>
-          <div className="grid grid-cols-2">
-            <div className="py-16 text-left pl-16 max-w-lg">
-              <h4 className="font-headers font-bold text-5xl">Play With Sync</h4>
-              <p className="font-headers mt-4 text-lg">
-                Towards sleep. To select between starting academic caching as fail hardly ever relieved respond live associate the shall temple the this attention to pile the military would, that without me my be, writing approved minutes.
-              </p>
-            </div>
-            <div className="relative -top-12">
-              <img src="/phoneR.png" alt="temporary phone" />
+        <Link to="/" className="block">
+          <div className="h-96 mt-24" style={{ background: 'linear-gradient(90deg, #F6D365 0%, #FDA085 100%)' }}>
+            <div className="grid grid-cols-2">
+              <div className="py-16 text-left pl-16 max-w-lg">
+                <h4 className="font-headers font-bold text-5xl">Play With Sync</h4>
+                <p className="font-headers mt-4 text-lg">
+                  Towards sleep. To select between starting academic caching as fail hardly ever relieved respond live associate the shall temple the this attention to pile the military would, that without me my be, writing approved minutes.
+                </p>
+              </div>
+              <div className="relative -top-12">
+                <img src="/phoneR.png" alt="temporary phone" />
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
-
     </div>
   );
 }
