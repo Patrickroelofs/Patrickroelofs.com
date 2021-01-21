95dc20d1f866601687a0a7a81b3b56b3a060c812
Patrick Roelofs <contact@patrickroelofs.com>
2021-01-21 01:52:23 +0100

    add better buttons

--- client/src/app/views/home/mywork/Mywork.jsx
+++ client/src/app/views/home/mywork/Mywork.jsx
@@ -6,19 +6,19 @@ function Mywork() {
   return (
     <div>
       <h2
-        className="text-center font-headers font-black relative top-24"
+        className="text-center font-headers font-black relative leading-none top-0 md:top-8 lg:top-16"
         style={{ fontSize: '14vw' }}
       >
         MY WORK
       </h2>
 
       <div className="relative z-10">
-        <div style={{ height: '30rem', background: 'linear-gradient(90deg, #84FAB0 0%, #8FD3F4 100%)' }}>
-          <div className="grid grid-cols-2">
+        <div className="md:h-auto" style={{ background: 'linear-gradient(90deg, #84FAB0 0%, #8FD3F4 100%)' }}>
+          <div className="grid grid-cols-1 md:grid-cols-2">
             <div className="relative -top-12">
               <img src="/phoneL.png" alt="temporary phone" />
             </div>
-            <div className="py-16 text-right pr-16 max-w-2xl">
+            <div className="py-0 pb-6 px-6 md:px-0 md:py-16 text-center md:text-right md:pr-16 max-w-2xl">
               <h4 className="font-headers font-bold text-5xl">Quizzer app</h4>
               <i className="font-headers font-medium text-sm">School Project</i>
               <p className="font-headers mt-4 text-lg">
@@ -37,9 +37,9 @@ function Mywork() {
           </div>
         </div>
 
-        <div className="mt-48" style={{ height: '30rem', background: 'linear-gradient(90deg, #F6D365 0%, #FDA085 100%)' }}>
-          <div className="grid grid-cols-2">
-            <div className="py-16 text-left pl-16 max-w-2xl">
+        <div className="mt-48 pt-16 md:pt-0 md:h-auto" style={{ background: 'linear-gradient(90deg, #F6D365 0%, #FDA085 100%)' }}>
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="py-0 pb-6 px-6 pl-6 md:pl-12 md:px-0 md:py-16 text-center md:text-right md:pr-16 max-w-2xl">
               <h4 className="font-headers font-bold text-5xl">Play With Sync</h4>
               <i className="font-headers font-medium text-sm">Personal Project</i>
               <p className="font-headers mt-4 text-lg block">
@@ -56,7 +56,7 @@ function Mywork() {
                 <img className="w-full transform duration-500 hover:scale-150 hover:shadow-2xl ease-in-out transition-all hover:z-50 z-40 relative rounded-md" src="https://picsum.photos/252" alt="image1" />
               </div>
             </div>
-            <div className="relative -top-12">
+            <div className="relative -top-12 hidden md:block">
               <img src="/phoneR.png" alt="temporary phone" />
             </div>
           </div>
