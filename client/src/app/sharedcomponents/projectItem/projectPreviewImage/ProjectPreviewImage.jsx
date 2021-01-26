d6465148bdcc35e9090b82ea0271dbc4a59de883
Patrick Roelofs <contact@patrickroelofs.com>
2021-01-27 00:04:39 +0100

    Fix previewImage bug

--- client/src/app/sharedcomponents/projectItem/projectPreviewImage/ProjectPreviewImage.jsx
+++ client/src/app/sharedcomponents/projectItem/projectPreviewImage/ProjectPreviewImage.jsx
@@ -4,14 +4,13 @@ import React from 'react';
 function ProjectPreviewImage({ url }) {
   const transition = {
     type: 'spring',
-    duration: 1,
+    duration: 0.75,
   };
 
   return (
     <motion.img
-      className="w-full rounded-md relative hover:z-10 hover:shadow-2xl cursor-pointer"
+      className="w-full rounded-md relative hover:z-50 hover:shadow-2xl"
       whileHover={{ scale: 1.5 }}
-      whileTap={{ scale: 1.2 }}
       transition={transition}
       key={url}
       src={url}
