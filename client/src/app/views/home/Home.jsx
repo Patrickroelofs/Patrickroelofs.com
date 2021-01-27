b58c551cf54606fd10e9a32bd2bd5ac3f41cfb2e
Patrick Roelofs <contact@patrickroelofs.com>
2021-01-27 01:03:01 +0100

    Fix crash - wrong import

--- client/src/app/views/home/Home.jsx
+++ client/src/app/views/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from '../../sharedcomponents/footer/Footer';
+import Footer from '../../footer/Footer';
 import Header from './header/Header';
 
 function Home() {
