abab4b7b5695bea57f21431ed03f4fd936f43b45
Patrick Roelofs <contact@patrickroelofs.com>
2021-02-01 23:58:35 +0100

    Update footer links with better hover states

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function Footer() {
   const FooterLink = ({ url, name }) => (
-    <span className="hover:bg-themeRed transition-colors inline-block mx-16">
-      <a target="_blank" rel="noreferrer" className="text-themeWhite font-headers font-black text-4xl px-4 py-2 inline-block" href={url}>{name}</a>
+    <span className="transition-all inline-block mx-16 transform hover:scale-125 ease-in-out group">
+      <a target="_blank" rel="noreferrer" className="text-themeWhite font-headers font-black text-4xl px-4 py-2 inline-block" href={url}>
+        {name}
+        <span className="group-hover:text-themeRed group-hover:opacity-100 opacity-0 ease-in-out transition-all duration-200">.</span>
+      </a>
     </span>
   );
 
