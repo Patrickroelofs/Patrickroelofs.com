d4ad2a1e3dcb267cc66bafc058c7ba2fd031a18c
Patrick Roelofs <contact@patrickroelofs.com>
2021-02-07 19:34:20 +0100

    fix metadata

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,21 +9,21 @@ import './stylesheet.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <div className="100vw min-h-screen">
-        <Helmet>
-          <title>Patrick Roelofs</title>
-          <meta name="description" content="Patrick Roelofs is a student webdeveloper in the Netherlands." />
-          <meta name="language" content="en" />
-          <meta name="robots" content="index,follow" />
-          <meta name="author" content="Patrick Roelofs, contact@patrickroelofs.com" />
-          <meta name="reply-to" content="contact@patrickroelofs.com" />
-          <meta name="url" content="https://patrickroelofs.com" />
+      <Helmet>
+        <title>Patrick Roelofs</title>
+        <meta name="description" content="Patrick Roelofs is a student webdeveloper in the Netherlands." />
+        <meta name="language" content="en" />
+        <meta name="robots" content="index,follow" />
+        <meta name="author" content="Patrick Roelofs, contact@patrickroelofs.com" />
+        <meta name="reply-to" content="contact@patrickroelofs.com" />
+        <meta name="url" content="https://patrickroelofs.com" />
 
-          <meta name="og:title" content="Patrick Roelofs" />
-          <meta name="og:type" content="portfolio" />
-          <meta name="og:url" content="https://patrickroelofs.com" />
-          <meta name="og:image" content="%PUBLIC_URL%/metadata/metaimage.png" />
-        </Helmet>
+        <meta name="og:title" content="Patrick Roelofs" />
+        <meta name="og:type" content="portfolio" />
+        <meta name="og:url" content="https://patrickroelofs.com" />
+        <meta name="og:image" content="%PUBLIC_URL%/metadata/metaimage.png" />
+      </Helmet>
+      <div className="100vw min-h-screen">
         <Router />
       </div>
     </BrowserRouter>
