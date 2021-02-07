fcebfca887454fc67ad22fcf57566470e0d04691
Patrick Roelofs <contact@patrickroelofs.com>
2021-02-07 19:28:38 +0100

    add metadata

--- client/src/components/skill/Skill.jsx
+++ client/src/components/skill/Skill.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Skill({ text, icon }) {
   return (
     <div className="p-4 m-4 rounded-lg bg-white shadow-xl text-center hover:shadow-2xl hover:scale-125 transform ease-in-out duration-200 transition-all cursor-default">
-      <span className="w-16 h-16 block">{icon}</span>
-      <p className="pt-4 font-headers">{text}</p>
+      <span className="w-16 h-16 block m-auto">{icon}</span>
+      <p className="pt-4 font-headers text-xs">{text}</p>
     </div>
   );
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './app/views/Router';
 
@@ -9,6 +10,20 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <div className="100vw min-h-screen">
+        <Helmet>
+          <title>Patrick Roelofs</title>
+          <meta name="description" content="Patrick Roelofs is a student webdeveloper in the Netherlands." />
+          <meta name="language" content="en" />
+          <meta name="robots" content="index,follow" />
+          <meta name="author" content="Patrick Roelofs, contact@patrickroelofs.com" />
+          <meta name="reply-to" content="contact@patrickroelofs.com" />
+          <meta name="url" content="https://patrickroelofs.com" />
+
+          <meta name="og:title" content="Patrick Roelofs" />
+          <meta name="og:type" content="portfolio" />
+          <meta name="og:url" content="https://patrickroelofs.com" />
+          <meta name="og:image" content="%PUBLIC_URL%/metadata/metaimage.png" />
+        </Helmet>
         <Router />
       </div>
     </BrowserRouter>
